f2db4853281683887823b005b3f0e6d480183ef1
Michael D. Hoyle <hoylemd@gmail.com>
2015-12-08 15:48:41 -0500

    add a test for the url builder

--- app/adapters/fivehundredpx.js
+++ app/adapters/fivehundredpx.js
@@ -1,15 +1,14 @@
 import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
-  host: 'https://api.501px.com',
+  host: 'https://api.500px.com',
   namespace: 'v1',
 
   buildUrl: function() {
-    // ihttps://api.500px.com/v1/photos?feature=popular&image_size=&rpp=100
     var modelName = 'photos',
       feature = 'popular',
       rpp = 100,
-      url = host + '/' + namespace + '/' + modelName;
+      url = this.get('host') + '/' + this.get('namespace') + '/' + modelName;
 
     return url + '?feature=' + feature + '&rpp=' + rpp;
   }
--- tests/unit/adapters/fivehundredpx-test.js
+++ tests/unit/adapters/fivehundredpx-test.js
@@ -1,12 +1,16 @@
 import { moduleFor, test } from 'ember-qunit';
 
 moduleFor('adapter:fivehundredpx', 'Unit | Adapter | fivehundredpx', {
-  // Specify the other units that are required for this test.
-  // needs: ['serializer:foo']
 });
 
-// Replace this with your real tests.
 test('it exists', function(assert) {
   let adapter = this.subject();
   assert.ok(adapter);
+});
+
+test('buildUrl builds a 500px photos query url', function(assert) {
+  let adapter = this.subject(),
+    expected_url = "https://api.500px.com/v1/photos?feature=popular&rpp=100";
+
+  assert.ok(adapter.buildUrl() === expected_url, "Url was built correctly");
 });
