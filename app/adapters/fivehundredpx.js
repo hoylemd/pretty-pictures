9870937b902c1f9739d75357097e14ff5dcc23c1
Michael D. Hoyle <hoylemd@gmail.com>
2015-12-08 15:44:00 -0500

    add some adapter content

--- app/adapters/fivehundredpx.js
+++ app/adapters/fivehundredpx.js
@@ -1,4 +1,17 @@
 import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
+  host: 'https://api.501px.com',
+  namespace: 'v1',
+
+  buildUrl: function() {
+    // ihttps://api.500px.com/v1/photos?feature=popular&image_size=&rpp=100
+    var modelName = 'photos',
+      feature = 'popular',
+      rpp = 100,
+      url = host + '/' + namespace + '/' + modelName;
+
+    return url + '?feature=' + feature + '&rpp=' + rpp;
+  }
+
 });
